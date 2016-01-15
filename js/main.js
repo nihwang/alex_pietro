3901fd4ce4ed6e291def95d2717873b5ede9ff6f
Nina Hwang <nihwang12@gmail.com>
2016-01-15 12:13:57 -0800

    Added justified gallery and fresco

---
+++ js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100) {
+			$('.nav-bar').addClass('fix-top');
+		}
+		else {
+			$('.nav-bar').removeClass('fix-top');
+		}
+	});
+
+	$('#mygallery').justifiedGallery({
+		rowHeight: 175,
+		maxRowHeight: '200%',
+		lastRow: 'nojustify',
+		margins: 10,
+		randomize: true
+	}).on('jg.complete', function() {
+		
+	});
+});
